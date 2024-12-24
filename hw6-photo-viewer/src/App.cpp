21a75f6a8e2e0dc3dd2112ed6af52c584789e934
Kabanya <evonirazh@mail.ru>
2024-12-24 20:13:44 +0300

    sfml 3.0 версии + CMakeLists без костыля

--- hw6-photo-viewer/src/App.cpp
+++ hw6-photo-viewer/src/App.cpp
@@ -1,18 +1,18 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
-int main() {
+int main()
+{
+    sf::Window window(sf::VideoMode({800, 600}), "My window");
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test Window");
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        while (const std::optional event = window.pollEvent())
+        {
+            // "close requested" event: we close the window
+            if (event->is<sf::Event::Closed>())
                 window.close();
         }
-        window.clear(sf::Color::Black);
-        window.display();
     }
-
-    return 0;
 }
