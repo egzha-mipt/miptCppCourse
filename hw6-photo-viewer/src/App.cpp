059ac389c7f699eefbca8383e3d96d467d07cabd
Kabanya <evonirazh@mail.ru>
2024-12-24 20:46:55 +0300

    наруто с шион выводятся

--- hw6-photo-viewer/src/App.cpp
+++ hw6-photo-viewer/src/App.cpp
@@ -1,18 +1,27 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::Window window(sf::VideoMode({800, 600}), "My window");
+    sf::RenderWindow window(sf::VideoMode({1200, 600}), "Window");
 
-    // run the program as long as the window is open
+    sf::Texture texture("shion_and_naruto_wallpaper.bmp");
+
+    sf::Sprite sprite(texture);
+
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
-        while (const std::optional event = window.pollEvent())
+       while (const std::optional event = window.pollEvent())
         {
-            // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
+
+        window.clear(sf::Color::Black);
+
+        window.draw(sprite);
+
+        window.display();
     }
+
+    return 0;
 }
