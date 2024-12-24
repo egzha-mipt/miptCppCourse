6b3df1aeffbce575ed64c7b3421c1c0e545e4013
Kabanya <evonirazh@mail.ru>
2024-12-24 23:06:27 +0300

    +-3 пункт Done: позиция мыши + N

--- hw6-photo-viewer/src/App.cpp
+++ hw6-photo-viewer/src/App.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include <fstream>
 
 void log(const std::string& message, std::ofstream& logFile)
@@ -11,9 +12,9 @@ int main()
     std::ofstream logFile("log.txt", std::ios::out);
 
     sf::RenderWindow window(sf::VideoMode({1200, 600}), "Window");
-
+    
+    //Texture
     sf::Texture texture("shion_and_naruto_wallpaper.bmp");
-
     sf::Sprite sprite(texture);
 
     while (window.isOpen())
@@ -24,7 +25,15 @@ int main()
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
-        
+
+        sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+        log("Позиция мыши: " + std::to_string(mousePosition.x) + ", " + std::to_string(mousePosition.y), logFile);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::N))
+        {
+            log("Клавиша N нажата", logFile);
+        }
+
         window.clear(sf::Color::Black);
             log("Черный фон", logFile); 
 
